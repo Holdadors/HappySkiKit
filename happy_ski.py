ee7d448ce49d5a08080b03bed150a2a8beb0f591
Holdadors <127192476+Holdadors@users.noreply.github.com>
2024-04-13 04:07:19 -0400

    Add files via upload
    Uploading Better Data with more information, class file (pseudo code), and ml model running

---
+++ happy_ski.py
@@ -0,0 +1,36 @@
+"""
+Jiajun Fang
+DS 5010 Final Project
+4/12/2024
+
+"""
+
+
+# PSEUDO CODE
+
+class happyski:
+    """
+    Import data
+    Update data
+    Train model
+    Make ratings
+    """
+    def __init__(self, model, dataset):
+        """Have default models and dataset ready"""
+        self.model = model
+        self.dataset = dataset
+
+    def import_data(self, dataset):
+        """Import and clean data for modelling"""
+
+    def train(self, model):
+        """Train best model to predict rating"""
+
+    def predict(self):
+        """Make a prediction based on input data"""
+
+    def update(self, dataset):
+        """Allow user to update data by giving their own scores"""
+
+
+
